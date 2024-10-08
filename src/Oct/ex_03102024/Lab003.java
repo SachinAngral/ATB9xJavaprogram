72bc4682aff6c6e08b2158efc6b7e7b05b4e13bc
SachinAngral <sachinangral@gmail.com>
2024-10-07 20:33:12 -0400

    Base Project added

--- src/Oct/ex_03102024/Lab003.java
+++ src/Oct/ex_03102024/Lab003.java
@@ -1,4 +1,14 @@
 package Oct.ex_03102024;
 
 public class Lab003 {
+    public static void main(String[] args) {
+        int a=20;
+        int b=10;
+        System.out.println(a+b);
+        System.out.println(a-b);
+        System.out.println(a*b);
+        System.out.println(a/b);
+        System.out.println(a%b);
+
+    }
 }
